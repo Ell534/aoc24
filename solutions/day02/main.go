393770e986f585d0cd1762024ac35e3983488480
Ell534 <elliot94b@gmail.com>
2024-12-04 17:03:31 +0000

    solve day02 part 2

--- solutions/day02/main.go
+++ solutions/day02/main.go
@@ -15,9 +15,11 @@ const TEST = "../../inputs/day02Test.txt"
 func main() {
 	inputData := readFile(FILE)
 	convertedData := convertToInt(inputData)
-	fmt.Println(convertedData[0])
-	totalSafeReports := checkIfSafe(convertedData)
-	fmt.Println(totalSafeReports)
+	totalSafeReports := countSafeReports(convertedData)
+	fmt.Printf("Part 1 Answer: %d\n", totalSafeReports)
+	totalDampenedSafeReports := dampenReports(convertedData)
+	fmt.Printf("Part 2 Answer: %d\n", totalDampenedSafeReports)
+
 }
 
 func readFile(filePath string) []string {
@@ -36,18 +38,12 @@ func readFile(filePath string) []string {
 	return data
 }
 
-// split each element on " " into their individual string numbers
-// convert each string number into integer and put into a slice
-// put that slice of ints into a slice containing all the slices of ints i.e [][]int
-// run checks to see if it meets the conditions: must be either all increasing or all decreasing, AND each adjacent number can only differ by at least one AND at most 3
-// check for all increasing or decreasing
-
 func convertToInt(data []string) [][]int {
 	convertedData := [][]int{}
 	for _, line := range data {
 		intNumberReport := []int{}
 		stringNumbersReport := strings.Split(line, " ")
-		//convert to ints
+
 		for _, number := range stringNumbersReport {
 			intValue, _ := strconv.Atoi(number)
 			intNumberReport = append(intNumberReport, intValue)
@@ -57,52 +53,82 @@ func convertToInt(data []string) [][]int {
 	return convertedData
 }
 
-func checkIfSafe(reports [][]int) int {
-	safeReports := 0
-	for _, report := range reports {
-		safe := true
+func checkIfSafe(report []int) bool {
 
-		var direction string
-		if report[0] < report[1] {
-			direction = "up"
-		} else if report[0] > report[1] {
-			direction = "down"
+	safe := true
+
+	var direction string
+	if report[0] < report[1] {
+		direction = "up"
+	} else if report[0] > report[1] {
+		direction = "down"
+	} else {
+		safe = false
+		return safe
+	}
+
+	// check if all values are either moving up or down in value, if not set safe to false and continue to next report
+	for i := 0; i < len(report)-1; i++ {
+		currentDirection := ""
+		if report[i] < report[i+1] {
+			currentDirection = "up"
+		} else if report[i] > report[i+1] {
+			currentDirection = "down"
+		}
+		if direction != currentDirection {
+			safe = false
+			break
+		}
+	}
+
+	// check for differences in value, at least 1 and no more than 3
+	for i := 0; i < len(report)-1; i++ {
+		difference := report[i] - report[i+1]
+		if difference < 0 {
+			difference = -difference
+		}
+		if difference > 0 && difference <= 3 {
+			continue
 		} else {
 			safe = false
+			break
 		}
+	}
 
-		// check for all either moving up or down in value, if not set safe to false and continue to next report
-		for i := 0; i < len(report)-1; i++ {
-			currentDirection := ""
-			if report[i] < report[i+1] {
-				currentDirection = "up"
-			} else if report[i] > report[i+1] {
-				currentDirection = "down"
-			}
-			if direction != currentDirection {
-				safe = false
-				break
-			}
-		}
+	return safe
+}
 
-		// check for differences in value, at least 1 and no more than 3
-		for i := 0; i < len(report)-1; i++ {
-			difference := report[i] - report[i+1]
-			if difference < 0 {
-				difference = -difference
-			}
-			if difference > 0 && difference <= 3 {
-				continue
-			} else {
-				safe = false
-				break
-			}
-		}
-
-		if safe == true {
+func countSafeReports(reports [][]int) int {
+	safeReports := 0
+	for _, report := range reports {
+		if checkIfSafe(report) {
 			safeReports++
+		}
+	}
+	return safeReports
+}
+
+// for each report, remove one element and see if it is made safe, if so add one to the count and move on to the next report, if not remove the next element and check again etc
+func dampenReports(reports [][]int) int {
+	totalDampened := 0
+
+	for _, report := range reports {
+		dampened := false
+
+		for i := 0; i < len(report); i++ {
+			testSlice := make([]int, 0, len(report)-1)
+			testSlice = append(testSlice, report[:i]...)
+			testSlice = append(testSlice, report[i+1:]...)
+
+			if checkIfSafe(testSlice) {
+				dampened = true
+			}
+		}
+
+		if dampened {
+			totalDampened++
 		}
 
 	}
-	return safeReports
+	return totalDampened
 }
